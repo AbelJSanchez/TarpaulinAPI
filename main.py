0d7267de34789cd54d6542f539071b96b7068369
AbelJSanchez <abels1@pm.me>
2025-11-30 11:09:53 -0800

    Implemented function _verify_jwt()

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 import json
 
+from typing import Any
+
 from six.moves.urllib.request import urlopen
+
 from jose import jwt
 from authlib.integrations.flask_client import OAuth
 from google.cloud import datastore
+
 from flask import Flask, request, jsonify
 
 CLIENT_ID = 'X'
@@ -29,22 +33,151 @@ auth0 = oauth.register(
     }
 )
 
+# Required Fields
+USER_FIELDS = ["Username", "Password"]
 
-def _valid_request():
-    pass
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
 
 
-def _handle_auth_error():
-    pass
+@app.errorhandler(AuthError)
+def _handle_auth_error(ex):
+    """ Handle the AuthError by returning a JSON response to the client. """
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+
+def _verify_request(body: dict[str, Any], fields: list) -> bool:
+    """ Verify that an incoming request contains the required fields """
+    if not body or len(body) < len(fields):
+        return False
+
+    for field in fields:
+        if field not in body:
+            return False
+
+    return True
+
+
+def get_token(request):
+    """ Retrieve the JWT from the authorization header. """
+    if 'Authorization' in request.headers:
+        auth_header = request.headers['Authorization'].split()
+        token = auth_header[1]
+    else:
+        raise AuthError(
+            {
+                "code": "no_auth_header",
+                "description": "Authorization header is missing"},
+            401
+        )
+
+    return token
+
+
+def verify_signature(token):
+    """ Verify that the client contains an RS256 signed JWT. """
+    try:
+        unverified_header = jwt.get_unverified_header(token)
+    except jwt.JWTError:
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Invalid header. Use an RS256 signed JWT Access Token"},
+            401
+        )
+
+    if unverified_header["alg"] == "HS256":
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Invalid header. Use an RS256 signed JWT Access Token"},
+            401
+        )
+
+    return unverified_header
+
+
+def build_rsa_key(unverified_header):
+    """ Retrieve the RSA key from the JWKS endpoint. """
+    jsonurl = urlopen(
+        "https://" + DOMAIN + "/.well-known/jwks.json"
+    )
+    jwks = json.loads(jsonurl.read())
+
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+
+    return rsa_key
+
+
+def verify_rsa_key(rsa_key, token):
+    """ Verify that the RSA Key is valid. """
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms="RSA256",
+                audience=CLIENT_ID,
+                issuer="https://" + DOMAIN + "/"
+            )
+        except jwt.ExpiredSignatureError:
+            raise AuthError(
+                {
+                    "code": "token_expired",
+                    "description": "Token is expired"
+                },
+                401
+            )
+        except jwt.JWTClaimsError:
+            raise AuthError(
+                {
+                    "code": "invalid_claims",
+                    "description": "Incorrect claims, please check the audience and issuer"
+                },
+                401
+            )
+        except Exception:
+            raise AuthError(
+                {
+                    "code": "invalid_header",
+                    "description": "Unable to parse authentication token."},
+                401
+            )
+        return payload
+
+    raise AuthError(
+        {
+            "code": "no_rsa_key",
+            "description": "No RSA key in JWKS"},
+        401
+    )
 
 
 def _verify_jwt():
-    pass
+    """ Verify that the client has a valid JWT. """
+    token = get_token(request)
+    unverified_header = verify_signature(token)
+    rsa_key = build_rsa_key(unverified_header)
+    return verify_rsa_key(rsa_key, token)
 
 
 @app.route('/')
 def index():
-    pass
+    return "Please provide a resource path to use the API."
 
 
 @app.route('/users/login', methods=['POST'])
